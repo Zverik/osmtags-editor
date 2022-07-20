9611288467349b005c60659a2c02a5809670d12f
Ilya Zverev <ilya@zverev.info>
2022-07-20 12:32:15 +0300

    Added 128x128 icon and a screenshot

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ and relation pages on the [osm.org](https://www.openstreetmap.org) website.
 The button opens a text area for editing raw tags (in form `key=value`) with
 a "Save" button to upload changes.
 
+![Screenshot of the editor in action](osmtags-editor-screen.png)
+
 ## Author and License
 
 Written by Ilya Zverev, published under MIT license.
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -eu
 rm osmtags-firefox.zip osmtags-chrome.zip
 mkdir tmp
-cp manifest.json osmorg-editor.js sidebar-listener.js land.html osm-auth.iife.min.js osmtags-48.png osmtags-96.png LICENSE README.md tmp
+cp manifest.json osmorg-editor.js sidebar-listener.js land.html osm-auth.iife.min.js osmtags-48.png osmtags-96.png osmtags-128.png LICENSE README.md tmp
 cd tmp
 zip -q ../osmtags-firefox.zip *
 cp ../manifest3.json manifest.json
